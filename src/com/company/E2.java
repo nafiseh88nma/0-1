2163dd175296afeb93527ed93d1aebec9110e332
nafiseh <fun101.nm@gmail.com>
2020-01-29 12:47:54 +0330

    homework0-1

---
+++ src/com/company/E2.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by admin on 9/19/2019.
+ */
+public class E2 {
+    public static void main(String[] args) {
+
+
+            if(true)
+            {
+                System.out.print("hellow");
+            }else
+            {
+                System.out.print("bye");
+            }
+
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int i=0;
+        if(i<3)
+        {
+            System.out.print("*");
+        }else if (i<6)
+        {
+            System.out.print("+");
+        }
+
+    }
+}
